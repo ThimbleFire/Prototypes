9c35f5fa310ba011c5b8a401a1eb63ac8e6e7bc7
Thimble <anthonyeboothroyd@gmail.com>
2021-01-30 03:54:15 +0000

    Create Binary.cs

---
+++ Binary.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Binary
+{
+    public static int ToDecimal( string binary )
+    {
+        return Convert.ToInt32( binary, 2 );
+    }
+
+    //
+    public static int ToDecimal( string binary, int propertyIndex )
+    {
+        string snippet = binary.Substring( propertyIndex * 8, 8 );
+        return Convert.ToInt32( snippet, 2 );
+    }
+
+    public static string ToBinary( int v )
+    {
+        return Convert.ToString( v, 2 ).PadLeft( 8, '0' );
+    }
+}
